f1ba272cfc6f49ffedcf0b10fe17d069584196fd
LakeBarker <winterwolf52@gmail.com>
2022-11-29 19:26:23 -0500

    assignment finished

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,30 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+class BankAccount:
+  def __init__(self, owner, balance, interestRate):
+    self.owner = owner
+    self.balance = balance or 0
+    self.interestRate = interestRate or .02
+
+  def __str__(self):
+    return"{}'s bank account currently has ${} in it".format(self.owner, self.balance)
+  
+  def deposit(self):
+    deposit_amount = input("Please record how much will be deposited ")
+    if (deposit_amount>=0):
+      self.balance = self.balance + deposit_amount
+      return"{}'s bank account now contains ${}".format(self.owner, self.balance)
+    else :
+      return False + "While we appreciate the idea of a gift, we do not accept them"
+  
+  def withdraw(self):
+    withdraw_amount = input("How much will you be withdrawing? ")
+    if (withdraw_amount > self.balance or withdraw_amount < 0):
+      return False + "You are unable to withdraw more than is in your account, or withdraw negative amounts"
+    else :   
+      self.balance = self.balance - withdraw_amount
+
+basic_account = BankAccount
+print(__str__)
+
+print("Have a nice day!")
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,21 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+string = "Berserk"
+
+li = []
+l = len(string)
+
+for i in range (0,1):
+    li.append(string[i])
+
+for i in range(0,1):
+    for j in range(0,1):
+        if li[i]<li[j]:
+            li[i],li[j]=li[j],li[i]
+j=""
+
+for i in range(0,1):
+    j=j+li[i]
+
+print(j)
